9cbd3a26e4d7d73033939e893526890dde61f3fd
bitmapdata <bitmapdata.com@gmail.com>
2013-07-18 03:00:39 +0900

    Updated Sample code.

--- MSCellAccessoryDemo/TBViewController.m
+++ MSCellAccessoryDemo/TBViewController.m
@@ -43,7 +43,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 11;
+    return 8;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -53,56 +53,106 @@
     if (cell == nil)
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
     
+    cell.textLabel.font = [UIFont systemFontOfSize:12.0];
     if(indexPath.row == 0)
     {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:DISCLOSURE_INDICATOR color:[UIColor colorWithRed:253/255.0 green:184/255.0 blue:0/255.0 alpha:1.0]];
+        cell.textLabel.text = @"MSCellAccessory / DETAL_DISCLOSURE";
+        [self configureCell:cell indexPath:indexPath accessoryType:DETAIL_DISCLOSURE];
     }
     else if(indexPath.row == 1)
     {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:DISCLOSURE_INDICATOR color:[UIColor colorWithRed:132/255.0 green:100/255.0 blue:159/255.0 alpha:1.0]];
+        cell.textLabel.text = @"UITableViewCellAccessoryDetailDisclosureButton";
+        cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
     }
     else if(indexPath.row == 2)
     {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:DISCLOSURE_INDICATOR color:[UIColor colorWithRed:0/255.0 green:166/255.0 blue:149/255.0 alpha:1.0]];
+        cell.textLabel.text = @"MSCellAccessory / DISCLOSURE_INDICATOR";
+        [self configureCell:cell indexPath:indexPath accessoryType:DISCLOSURE_INDICATOR];
     }
     else if(indexPath.row == 3)
     {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:DISCLOSURE_INDICATOR color:[UIColor colorWithRed:44/255.0 green:44/255.0 blue:44/255.0 alpha:1.0]];
+        cell.textLabel.text = @"UITableViewCellAccessoryDisclosureIndicator";
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     }
     else if(indexPath.row == 4)
     {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:DISCLOSURE_INDICATOR color:[UIColor colorWithRed:234/255.0 green:87/255.0 blue:123/255.0 alpha:1.0]];
+        cell.textLabel.text = @"MSCellAccessory / CHECKMARK";
+        [self configureCell:cell indexPath:indexPath accessoryType:CHECKMARK];
     }
     else if(indexPath.row == 5)
     {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:DISCLOSURE_INDICATOR color:[UIColor colorWithRed:35/255.0 green:110/255.0 blue:216/255.0 alpha:1.0]];
+        cell.textLabel.text = @"UITableViewCellAccessoryCheckmark";
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
     }
     else if(indexPath.row == 6)
     {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:DISCLOSURE_INDICATOR color:[UIColor colorWithRed:128/255.0 green:0/255.0 blue:64/255.0 alpha:1.0]];
+        cell.textLabel.text = @"MSCellAccessory / TOGGLE_INDICATOR";
+        [self configureCell:cell indexPath:indexPath accessoryType:TOGGLE_INDICATOR];
+        MSCellAccessory *acc = (MSCellAccessory *)cell.accessoryView;
+        acc.selected = true;
     }
     else if(indexPath.row == 7)
     {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:DISCLOSURE_INDICATOR color:[UIColor colorWithRed:0/255.0 green:128/255.0 blue:0/255.0 alpha:1.0]];
-
-    }
-    else if(indexPath.row == 8)
-    {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:DISCLOSURE_INDICATOR color:[UIColor colorWithRed:175/255.0 green:106/255.0 blue:0/255.0 alpha:1.0]];
-
-    }
-    else if(indexPath.row == 9)
-    {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:DISCLOSURE_INDICATOR color:[UIColor colorWithRed:200/255.0 green:63/255.0 blue:67/255.0 alpha:1.0]];
-
-    }
-    else if(indexPath.row == 10)
-    {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:DISCLOSURE_INDICATOR color:[UIColor colorWithRed:255/255.0 green:51/255.0 blue:102/255.0 alpha:1.0]];
-
+        cell.textLabel.text = @"MSCellAccessory / TOGGLE_INDICATOR";
+        [self configureCell:cell indexPath:indexPath accessoryType:TOGGLE_INDICATOR];
+        MSCellAccessory *acc = (MSCellAccessory *)cell.accessoryView;
+        acc.selected = false;
     }
     
     return cell;
+}
+
+- (void)configureCell:(UITableViewCell *)cell indexPath:(NSIndexPath *)indexPath accessoryType:(AccessoryType )accessoryType
+{
+    if(indexPath.row == 0)
+    {
+        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:35/255.0 green:110/255.0 blue:216/255.0 alpha:1.0]];
+    }
+    else if(indexPath.row == 1)
+    {
+        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:132/255.0 green:100/255.0 blue:159/255.0 alpha:1.0]];
+    }
+    else if(indexPath.row == 2)
+    {
+        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:127/255.0 green:127/255.0 blue:127/255.0 alpha:1.0]];
+    }
+    else if(indexPath.row == 3)
+    {
+        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:44/255.0 green:44/255.0 blue:44/255.0 alpha:1.0]];
+    }
+    else if(indexPath.row == 4)
+    {
+        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:50/255.0 green:79/255.0 blue:133/255.0 alpha:1.0] highlightedColor:[UIColor whiteColor]];
+    }
+    else if(indexPath.row == 5)
+    {
+        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:35/255.0 green:110/255.0 blue:216/255.0 alpha:1.0]];
+    }
+    else if(indexPath.row == 6)
+    {
+        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:0/255.0 green:123/255.0 blue:170/255.0 alpha:1.0]];
+    }
+    else if(indexPath.row == 7)
+    {
+        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:0/255.0 green:123/255.0 blue:170/255.0 alpha:1.0]];
+        
+    }
+    else if(indexPath.row == 8)
+    {
+        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:175/255.0 green:106/255.0 blue:0/255.0 alpha:1.0]];
+        
+    }
+    else if(indexPath.row == 9)
+    {
+        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:200/255.0 green:63/255.0 blue:67/255.0 alpha:1.0]];
+        
+    }
+    else if(indexPath.row == 10)
+    {
+        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:255/255.0 green:51/255.0 blue:102/255.0 alpha:1.0]];
+        
+    }
+
 }
 
 - (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
@@ -110,57 +160,18 @@
     cell.backgroundColor = [UIColor whiteColor];
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
 #pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView deselectRowAtIndexPath:indexPath animated:NO];
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
+    
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    if(indexPath.row == 6 || indexPath.row == 7)
+    {
+        MSCellAccessory *tmp = (MSCellAccessory *)cell.accessoryView;
+        tmp.selected =! tmp.selected;
+    }
 }
 
 @end
