e18433533128504cefc31d22f3b29cc878f218e1
bitmapdata <bitmapdata.com@gmail.com>
2013-07-20 01:46:55 +0900

    Updated sample code

--- MSCellAccessoryDemo/TBViewController.m
+++ MSCellAccessoryDemo/TBViewController.m
@@ -19,7 +19,6 @@
 {
     self = [super initWithStyle:style];
     if (self) {
-        // Custom initialization
     }
     return self;
 }
@@ -43,7 +42,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 8;
+    return 11;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -53,106 +52,113 @@
     if (cell == nil)
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
     
-    cell.textLabel.font = [UIFont systemFontOfSize:12.0];
-    if(indexPath.row == 0)
-    {
-        cell.textLabel.text = @"MSCellAccessory / DETAL_DISCLOSURE";
-        [self configureCell:cell indexPath:indexPath accessoryType:DETAIL_DISCLOSURE];
-    }
-    else if(indexPath.row == 1)
-    {
-        cell.textLabel.text = @"UITableViewCellAccessoryDetailDisclosureButton";
-        cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
-    }
-    else if(indexPath.row == 2)
-    {
-        cell.textLabel.text = @"MSCellAccessory / DISCLOSURE_INDICATOR";
-        [self configureCell:cell indexPath:indexPath accessoryType:DISCLOSURE_INDICATOR];
-    }
-    else if(indexPath.row == 3)
-    {
-        cell.textLabel.text = @"UITableViewCellAccessoryDisclosureIndicator";
-        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-    }
-    else if(indexPath.row == 4)
-    {
-        cell.textLabel.text = @"MSCellAccessory / CHECKMARK";
-        [self configureCell:cell indexPath:indexPath accessoryType:CHECKMARK];
-    }
-    else if(indexPath.row == 5)
-    {
-        cell.textLabel.text = @"UITableViewCellAccessoryCheckmark";
-        cell.accessoryType = UITableViewCellAccessoryCheckmark;
-    }
-    else if(indexPath.row == 6)
-    {
-        cell.textLabel.text = @"MSCellAccessory / TOGGLE_INDICATOR";
-        [self configureCell:cell indexPath:indexPath accessoryType:TOGGLE_INDICATOR];
-        MSCellAccessory *acc = (MSCellAccessory *)cell.accessoryView;
-        acc.selected = true;
-    }
-    else if(indexPath.row == 7)
-    {
-        cell.textLabel.text = @"MSCellAccessory / TOGGLE_INDICATOR";
-        [self configureCell:cell indexPath:indexPath accessoryType:TOGGLE_INDICATOR];
-        MSCellAccessory *acc = (MSCellAccessory *)cell.accessoryView;
-        acc.selected = false;
-    }
+    cell.textLabel.font = [UIFont systemFontOfSize:10.0];
+    
+    [self configureCell:cell indexPath:indexPath accessoryType:FLAT_DETAIL_DISCLOSURE];
+//    [self configureCell:cell indexPath:indexPath accessoryType:FLAT_DETAIL_BUTTON];
+//    [self configureCell:cell indexPath:indexPath accessoryType:FLAT_DISCLOSURE_INDICATOR];
+//    [self configureCell:cell indexPath:indexPath accessoryType:FLAT_CHECKMARK];
+//    [self configureCell:cell indexPath:indexPath accessoryType:FLAT_TOGGLE_INDICATOR];
     
     return cell;
 }
 
 - (void)configureCell:(UITableViewCell *)cell indexPath:(NSIndexPath *)indexPath accessoryType:(AccessoryType )accessoryType
 {
-    if(indexPath.row == 0)
+    if(accessoryType == FLAT_DETAIL_DISCLOSURE)
     {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:35/255.0 green:110/255.0 blue:216/255.0 alpha:1.0]];
+        if(indexPath.row == 0)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType colors:@[[UIColor colorWithRed:255/255.0 green:17/255.0 blue:95/255.0 alpha:1.0], [UIColor colorWithWhite:0.9 alpha:1.0]]];
+        }
+        else if(indexPath.row == 1)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType colors:@[[UIColor colorWithRed:208/255.0 green:44/255.0 blue:55/255.0 alpha:1.0], [UIColor colorWithWhite:0.8 alpha:1.0]]];
+        }
+        else if(indexPath.row == 2)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType colors:@[[UIColor colorWithRed:245/255.0 green:142/255.0 blue:4/255.0 alpha:1.0], [UIColor colorWithWhite:0.7 alpha:1.0]]];
+        }
+        else if(indexPath.row == 3)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType colors:@[[UIColor colorWithRed:230/255.0 green:241/255.0 blue:5/255.0 alpha:1.0], [UIColor colorWithWhite:0.6 alpha:1.0]]];
+        }
+        else if(indexPath.row == 4)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType colors:@[[UIColor colorWithRed:202/255.0 green:190/255.0 blue:1/255.0 alpha:1.0], [UIColor colorWithWhite:0.5 alpha:1.0]]];
+        }
+        else if(indexPath.row == 5)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType colors:@[[UIColor colorWithRed:140/255.0 green:255/255.0 blue:39/255.0 alpha:1.0], [UIColor colorWithWhite:0.4 alpha:1.0]]];
+        }
+        else if(indexPath.row == 6)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType colors:@[[UIColor colorWithRed:20/255.0 green:155/255.0 blue:55/255.0 alpha:1.0], [UIColor colorWithWhite:0.3 alpha:1.0]]];
+        }
+        else if(indexPath.row == 7)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType colors:@[[UIColor colorWithRed:0/255.0 green:170/255.0 blue:255/255.0 alpha:1.0], [UIColor colorWithWhite:0.2 alpha:1.0]]];
+        }
+        else if(indexPath.row == 8)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType colors:@[[UIColor colorWithRed:36/255.0 green:95/255.0 blue:222/255.0 alpha:1.0], [UIColor colorWithWhite:0.1 alpha:1.0]]];
+        }
+        else if(indexPath.row == 9)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType colors:@[[UIColor colorWithRed:90/255.0 green:43/255.0 blue:172/255.0 alpha:1.0], [UIColor colorWithWhite:0. alpha:1.0]]];
+        }
+        else if(indexPath.row == 10)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType colors:@[FLAT_DETAIL_BUTTON_DEFAULT_COLOR, FLAT_DISCLOSURE_INDICATOR_DEFAULT_COLOR]];
+        }
     }
-    else if(indexPath.row == 1)
+    else
     {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:132/255.0 green:100/255.0 blue:159/255.0 alpha:1.0]];
+        if(indexPath.row == 0)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:255/255.0 green:17/255.0 blue:95/255.0 alpha:1.0]];
+        }
+        else if(indexPath.row == 1)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:208/255.0 green:44/255.0 blue:55/255.0 alpha:1.0]];
+        }
+        else if(indexPath.row == 2)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:245/255.0 green:142/255.0 blue:4/255.0 alpha:1.0]];
+        }
+        else if(indexPath.row == 3)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:230/255.0 green:241/255.0 blue:5/255.0 alpha:1.0]];
+        }
+        else if(indexPath.row == 4)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:202/255.0 green:190/255.0 blue:1/255.0 alpha:1.0]];
+        }
+        else if(indexPath.row == 5)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:140/255.0 green:255/255.0 blue:39/255.0 alpha:1.0]];
+        }
+        else if(indexPath.row == 6)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:20/255.0 green:155/255.0 blue:55/255.0 alpha:1.0]];
+        }
+        else if(indexPath.row == 7)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:0/255.0 green:170/255.0 blue:255/255.0 alpha:1.0]];
+        }
+        else if(indexPath.row == 8)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:36/255.0 green:95/255.0 blue:222/255.0 alpha:1.0]];
+        }
+        else if(indexPath.row == 9)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:90/255.0 green:43/255.0 blue:172/255.0 alpha:1.0]];
+        }
+        else if(indexPath.row == 10)
+        {
+            cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:0/255.0 green:0/255.0 blue:0/255.0 alpha:1.0]];
+        }
     }
-    else if(indexPath.row == 2)
-    {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:127/255.0 green:127/255.0 blue:127/255.0 alpha:1.0]];
-    }
-    else if(indexPath.row == 3)
-    {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:44/255.0 green:44/255.0 blue:44/255.0 alpha:1.0]];
-    }
-    else if(indexPath.row == 4)
-    {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:50/255.0 green:79/255.0 blue:133/255.0 alpha:1.0] highlightedColor:[UIColor whiteColor]];
-    }
-    else if(indexPath.row == 5)
-    {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:35/255.0 green:110/255.0 blue:216/255.0 alpha:1.0]];
-    }
-    else if(indexPath.row == 6)
-    {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:0/255.0 green:123/255.0 blue:170/255.0 alpha:1.0]];
-    }
-    else if(indexPath.row == 7)
-    {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:0/255.0 green:123/255.0 blue:170/255.0 alpha:1.0]];
-        
-    }
-    else if(indexPath.row == 8)
-    {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:175/255.0 green:106/255.0 blue:0/255.0 alpha:1.0]];
-        
-    }
-    else if(indexPath.row == 9)
-    {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:200/255.0 green:63/255.0 blue:67/255.0 alpha:1.0]];
-        
-    }
-    else if(indexPath.row == 10)
-    {
-        cell.accessoryView = [MSCellAccessory accessoryWithType:accessoryType color:[UIColor colorWithRed:255/255.0 green:51/255.0 blue:102/255.0 alpha:1.0]];
-        
-    }
-
 }
 
 - (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
@@ -165,13 +171,6 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView deselectRowAtIndexPath:indexPath animated:NO];
-    
-    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
-    if(indexPath.row == 6 || indexPath.row == 7)
-    {
-        MSCellAccessory *tmp = (MSCellAccessory *)cell.accessoryView;
-        tmp.selected =! tmp.selected;
-    }
 }
 
 @end
